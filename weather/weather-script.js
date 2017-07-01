5dc1e55be90854a85fd4f11ac19e271887d8ad76
Alex Baies <baies.alexandru90@yahoo.ro>
2017-07-01 18:49:33 +0300

    weather app first version.

--- weather/weather-script.js
+++ weather/weather-script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
          var currentPath = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=13c5f087963ccabe717f773a5d0038fb`;
          var days5 = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}d&mode=json&cnt=7&units=metric&APPID=13c5f087963ccabe717f773a5d0038fb`;
          var forecast16 = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&APPID=13c5f087963ccabe717f773a5d0038fb`;
-        Promise.all([getData(currentPath), getData(days5), getData(forecast16)]).then(function(data){
+       /* Promise.all([getData(currentPath), getData(days5), getData(forecast16)]).then(function(data){
             
              var currentWeather = data[0],
                  forecastFor5Days = data[1],
@@ -31,6 +31,22 @@ $(document).ready(function(){
          catch(function(err){
             console.log(err);
          });
+        */
+         getData(currentPath).then(function(data){
+             console.log(data);
+             renderCurrentWeather(data);
+             return getData(days5);
+         })
+           .then(function(data5Days){
+              console.log(data5Days);
+              return getData(forecast16);
+         })
+           .then(function(data16Days){
+               console.log(data16Days);
+           }) 
+           .catch(function(err){
+              console.log(err);
+           });
      }
       function getData(currentPath){
 
@@ -63,23 +79,40 @@ $(document).ready(function(){
               timeStamp =  prevTime.getTime()/1000 + prevTime.getTimezoneOffset() * 60,
               apiKey = "AIzaSyAJCwxZ2Fa32qQtnPcv-MTVe5P-67cG2o8",
               hourPath = "https://maps.googleapis.com/maps/api/timezone/json?location="+timePath+"&timestamp="+timeStamp+"&key="+apiKey;
-             
+              countryPath = `https://restcountries.eu/rest/v2/alpha/${data.sys.country}`;
+
                /* local time of the location */
 
-          var currentTime = getData(hourPath);
+          var currentTime = Promise.all([getData(hourPath),getData(countryPath)]);
               currentTime.then(function(response){
-                var offsets =  response.dstOffset*1000 + response.rawOffset*1000;
+                  //console.log(response);
+                var respTime = response[0]; 
+                var country = response[1].name; 
+                var offsets =  respTime.dstOffset*1000 + respTime.rawOffset*1000;
                 var time = new Date(timeStamp * 1000 + offsets);
-      
+          
+
              /* render the data */
 
-                  var dateOutput = ` <p>${data.name}</p
-              <span>${days[time.getDay()]} </span> <span>${time.getHours()}:${time.getMinutes()} </span> 
-              <p>${data.weather[0].main} </p> 
-            <div id="icon-container"> 
-               <p>${icon} </p>        
-               <p>${parseInt(data.main.temp,10)}&deg;C </p>
-            </div>`;
+                  var dateOutput = ` <div id="weather-details">
+               <div id="weather-icon">   
+              ${icon} 
+               </div>  
+               <div id="location">   
+              <p>${data.name}</p>
+              <p>${country}</p>
+              <p>${data.weather[0].main} </p>
+              </div>
+              <div id="temp">
+                   <span>${days[time.getDay()]} </span> <span>${time.getHours()}:${time.getMinutes()} </span>        
+                  <p>${parseInt(data.main.temp,10)}&deg;C </p>
+              </div>
+           </div>   
+            <div id="temp-interval">
+                 <p> <img src="therm.png"> Max. Temp:${data.main.temp_max}</p>
+                 <p> <img src="therm.png"> Min. Temp:${data.main.temp_min} </p>
+            </div>
+            `;
 
             $("#currentDate").html(dateOutput);
             $("#details").html(detailsOutput); 
